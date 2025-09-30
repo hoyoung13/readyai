ce13a8b90cf66d75309160667379bc41314fc0b6
천호영 <chy8754@naver.com>
2025-09-30 10:48:51 +0900

    아 오류

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,17 @@
+
+val ffmpegGitHubUser: String? = (findProperty("gpr.user") as String?) ?: System.getenv("GPR_USER")
+val ffmpegGitHubToken: String? = (findProperty("gpr.key") as String?) ?: System.getenv("GPR_KEY")
 allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/arthenica/ffmpeg-kit")
+            credentials {
+                username = ffmpegGitHubUser ?: ""
+                password = ffmpegGitHubToken ?: ""
+            }
+        }
     }
 }
 
